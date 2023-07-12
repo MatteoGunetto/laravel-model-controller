33ab7492e890385fb25d26ea53eb4bc49e6e569c
MatteoGunetto <Matteogunetto@tiscali.it>
2023-07-12 15:16:59 +0200

    tentativi stampa in pagina

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -1,11 +1,16 @@
 <?php
-
 namespace App\Http\Controllers\Guest;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+// import Model
+use App\Models\Movie;
 class PageController extends Controller
 {
-    //
+    public function index () {
+        $movies= movie::all();
+        // prova dd per importazione corretta database
+        dd($movies);
+
+    return view('welcome', compact('movies'));
+}
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hello world!</h1>
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @vite('resources/js/app.js')
+
+        <title>Laravel Model Controller</title>
+    </head>
+    <body>
+
+    </body>
 </html>
